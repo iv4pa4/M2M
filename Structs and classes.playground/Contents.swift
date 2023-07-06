d6ed5154b94d65881a6e9f1fcff275c76a2cd725
Ivayla Panayotova <99187068+iv4pa4@users.noreply.github.com>
2023-07-06 15:23:28 +0300

    classes and structs

--- Structs and classes.playground/Contents.swift
+++ Structs and classes.playground/Contents.swift
@@ -49,6 +49,67 @@ struct Calculator {
     func sum() -> Int{
         return value1 + value2
     }
+    
+    mutating func changingValue1(newValue: Int){
+        value1 = newValue
+    }
 }
 var calc = Calculator(value1: 10, value2: 15)
 calc.sum()
+calc.changingValue1(newValue: 30)
+calc.sum()
+
+var str = "iva 1"
+str.count
+str.uppercased()
+
+struct Animal{
+   private var name:String
+    private var legs: Int
+    static var numOfAnimals = 0
+    init(name: String, legs: Int){
+        self.name = name
+        self.legs = legs
+        Animal.numOfAnimals += 1
+    }
+}
+
+var animal = Animal(name: "Ico", legs: 4)
+print(Animal.numOfAnimals)
+var animal1 = Animal(name: "Ico", legs: 4)
+print(Animal.numOfAnimals)
+var animal2 = Animal(name: "Ico", legs: 4)
+print(Animal.numOfAnimals)
+
+class AnimalClass{
+    var name: String
+    var legs: Int
+    var type: String
+    init(name: String, legs: Int, type: String){
+        self.name = name
+        self.legs = legs
+        self.type = type
+    }
+    func makeSound(){
+        print("makingSound")
+    }
+}
+
+var racoon = AnimalClass(name: "Racoon", legs: 4, type: "racoon")
+
+class Dogg: AnimalClass{
+    init(name: String){
+        super.init(name: name, legs: 4, type: "dog")
+    }
+    override func makeSound() {
+        print("Dog sounds")
+    }
+    
+    deinit{
+        print("\(self.name) was destroyed")
+    }
+}
+
+var dog2 = Dogg(name: "Ico")
+racoon.makeSound()
+dog2.makeSound()
