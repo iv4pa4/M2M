24d6ca87fa0c7dee128f0af8853c45222df440a5
Ivayla Panayotova <99187068+iv4pa4@users.noreply.github.com>
2023-07-10 10:42:52 +0300

    Creating the second project

---
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Ivayla  Panayotova on 10.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
